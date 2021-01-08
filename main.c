8ac0c35afb5fa63bbf0cc3603207619183d7bbd9
Remco Leijenaar <remco-leijenaar@live.com>
2021-01-08 14:15:24 +0100

    Q-Learning

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 #include "cubelib.h"
 
-#define NEPISODES 100000
+#define NEPISODES 1000
 #define EPS 0.05
 #define ALPHA 0.2
 #define GAMMA 0.95
@@ -41,62 +41,125 @@ int epsilonGreedy(float *a, int len, float epsilon) {
   return action;
 }
 
-void sarsa() {
+void getQ(Cube c, Library lib, float **Q) {
+  Tree tr;
+  int new = nodeInLibrary(c, lib, &tr);
+  if (new) {
+    tr->value = safeMalloc(NACTION * sizeof(float));
+    for (size_t j = 0; j < NACTION; j++) {
+      tr->value[j] = uniform(-1, 1);
+    }
+  }
+  *Q = tr->value;
+}
+
+void qLearning() {
   Library lib;
-  Tree tr, trNext;
+  Tree tr;
   Cube c, cNext;
+  float *Q, *QNext;
   int action[NACTION][SWAP];
 
   size_t i, j;
-  int new, a, aNext, iter;
+  int new, a, aNext, t;
   float r;
 
+  // Initialization
   initLibrary(&lib);
   initActions(action);
   initCube(&c);
   initCube(&cNext);
 
-  for (i = 0; i < NEPISODES; i++) {
-    r = iter = 0;
-    scrambleCube(c, 20, action);
-    new = nodeInLibrary(c, lib, &tr);
-    if (new) {
-      tr->value = safeMalloc(NACTION * sizeof(float));
-      for (j = 0; j < NACTION; j++) {
-        tr->value[j] = uniform(-1, 1);
-      }
-    }
-    a = epsilonGreedy(tr->value, NACTION, EPS);
+  // For the Terminal state s, for all actions a: Q(s,a) = 0
+  nodeInLibrary(c, lib, &tr);
+  tr->value = safeMalloc(NACTION * sizeof(float));
+  for (j = 0; j < NACTION; j++) {
+    tr->value[j] = 0;
+  }
 
-    while (r < 0.9) {
-      iter++;
+  for (i = 0; i < NEPISODES; i++) {
+    t = 0;
+    scrambleCube(c, 20, action);
+    r = isSolved(c) ? 1 : 0;
+
+    getQ(c, lib, &Q);
+
+    while (r < 0.99) {
+      t++;
+
+      a = epsilonGreedy(Q, NACTION, 0.1);
       setCube(c, cNext);
       turn(cNext, action[a]);
-      new = nodeInLibrary(cNext, lib, &trNext);
+      if (isSolved(cNext)) {
+        r = 1;
+      }
+
+      getQ(c, lib, &QNext);
+      aNext = argmax(QNext, NACTION);
+      Q[a] += ALPHA * (r + GAMMA * QNext[aNext] - Q[a]);
+      setCube(cNext, c);
+      a = aNext;
+      Q = QNext;
+    }
+    // if (i > NEPISODES - 100) {
+    printf("%d\n", t);
+    //}
+  }
+
+  freeCube(c);
+  freeLibrary(lib);
+}
+
+void sarsa() {
+  Library lib;
+  Tree tr;
+  Cube c, cNext;
+  float *Q, *QNext;
+  int action[NACTION][SWAP];
+
+  size_t i, j;
+  int new, a, aNext, t;
+  float r;
+
+  // Initialization
+  initLibrary(&lib);
+  initActions(action);
+  initCube(&c);
+  initCube(&cNext);
+
+  // For the Terminal state s, for all actions a: Q(s,a) = 0
+  nodeInLibrary(c, lib, &tr);
+  Q = tr->value;
+  Q = safeMalloc(NACTION * sizeof(float));
+  for (j = 0; j < NACTION; j++) {
+    Q[j] = 0;
+  }
+
+  for (i = 0; i < NEPISODES; i++) {
+    t = 0;
+    scrambleCube(c, 20, action);
+    r = isSolved(c) ? 1 : 0;
+
+    getQ(c, lib, &Q);
+    a = epsilonGreedy(Q, NACTION, EPS);
+
+    while (r < 0.99) {
+      t++;
+      setCube(c, cNext);
+      turn(cNext, action[a]);
       if (isSolved(c)) {
         r = 1;
       }
-      if (new) {
-        trNext->value = safeMalloc(NACTION * sizeof(float));
-        if (r == 1) {
-          for (j = 0; j < NACTION; j++) {
-            trNext->value[j] = 0;
-          }
-        } else {
-          for (j = 0; j < NACTION; j++) {
-            trNext->value[j] = uniform(-1, 1);
-          }
-        }
-      }
-      aNext = epsilonGreedy(trNext->value, NACTION, 0.1);
-      tr->value[a] += ALPHA * (r + GAMMA * trNext->value[aNext] - tr->value[a]);
+      getQ(c, lib, &QNext);
+      aNext = epsilonGreedy(QNext, NACTION, EPS);
+      Q[a] += ALPHA * (r + GAMMA * QNext[aNext] - Q[a]);
       setCube(cNext, c);
       a = aNext;
-      tr = trNext;
+      Q = QNext;
     }
-    if (i > NEPISODES - 100) {
-      printf("%d\n", iter);
-    }
+    // if (i > NEPISODES - 100) {
+    printf("%d\n", t);
+    //}
   }
 
   freeCube(c);
@@ -106,6 +169,7 @@ void sarsa() {
 int main(int argc, char const *argv[]) {
   srand(time(NULL));
   printf("Epsilon: %lf -- Alpha: %lf -- Gamma: %lf", EPS, ALPHA, GAMMA);
-  sarsa();
+  qLearning();
+  // sarsa();
   return 0;
 }
